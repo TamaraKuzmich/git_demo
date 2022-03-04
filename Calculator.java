ad5b40fce1f88364b9e3de3d9428406a26b5785f
TamaraKuzmich <tkpapply@gmail.com>
2022-03-04 16:37:54 -0500

    added Calculator

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+public class Calculator
+public static void main (String []args){
+
+
+public int summarize (int a, int b){
+return a + b;
+}
+
+public int subtract (int a, int b){
+return a - b;
+}
+
+public int multiple (int a, int b){
+return a * b;
+}
+
+public int multiple (int a, int b){
+return a / b;
+}
+}
+
