a4d5041001d9a159a68c9d2e54b5a86397235809
TamaraKuzmich <tkpapply@gmail.com>
2022-03-07 11:58:19 -0500

    song created

--- Calculator.java
+++ Calculator.java
@@ -3,19 +3,19 @@ public static void main (String []args){
 
 
 public int summarize (int a, int b){
-return a + b;
+return (a + b);
 }
 
 public int subtract (int a, int b){
-return a - b;
+return (a - b);
 }
 
 public int multiple (int a, int b){
-return a * b;
+return (a * b);
 }
 
 public int multiple (int a, int b){
-return a / b;
+return (a / b);
 }
 }
 
